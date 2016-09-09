4455a776f3130b67ffa3af6adf3a4b84a7510353
NicoleZA <nicole.orpen@gmail.com>
2016-09-09 10:15:08 +0200

    added appendArray

--- index.ts
+++ index.ts
@@ -16,81 +16,81 @@ class Tagging {
 	/** Create a new observable tag object
 	* If icon is left blank the default icon is used 
 	*/
-	public newTag(icon?: string ) : observableModule.Observable {
-		if(!icon) icon = this.unTagIcon;
-		return new observableModule.Observable({value:icon});
+	public newTag(icon?: string): observableModule.Observable {
+		if (!icon) icon = this.unTagIcon;
+		return new observableModule.Observable({ value: icon });
 	}
 
 	/** set all array objects tag property to the default tagged icon object */
-	public tagAll(array: any[]) : any[] {
+	public tagAll(array: any[]): any[] {
 		var me = this;
 		for (var i = 0; i < array.length; i++) {
-			array[i].tag = me.newTag(me.tagIcon); 
+			array[i].tag = me.newTag(me.tagIcon);
 		}
 		return array;
 	}
 	/** set all array objects tag property to the default untagged icon object */
-	public unTagAll(array: any[]) : any[] {
+	public unTagAll(array: any[]): any[] {
 		var me = this;
 		for (var i = 0; i < array.length; i++) {
-			array[i].tag = me.newTag(); 
+			array[i].tag = me.newTag();
 		}
 		return array;
 	}
 	/** get the toggled tag icon */
-	public toggleTagIcon(icon: string) : string {
-		if(icon == this.tagIcon) {
+	public toggleTagIcon(icon: string): string {
+		if (icon == this.tagIcon) {
 			return this.unTagIcon;
 		} else {
 			return this.tagIcon;
 		}
 	}
 	/** Toggle the rows tag property */
-	public toggleRow(row : any) : any {
-		if(!row) return null;
+	public toggleRow(row: any): any {
+		if (!row) return null;
 		row.tag = this.newTag(this.toggleTagIcon(row.tag));
 		return row;
 	}
 
 	/** Toggle the observable tag object */
-	public toggleObservable(obervableTag: observableModule.Observable) : observableModule.Observable {
+	public toggleObservable(obervableTag: observableModule.Observable): observableModule.Observable {
 		return this.newTag(this.toggleTagIcon(obervableTag.get("value")));
 	}
 	/** Toggle the observable rows tag object */
-	public toggleObservableRow(array: ObservableArray<any>, index : number) : ObservableArray<any> {
+	public toggleObservableRow(array: ObservableArray<any>, index: number): ObservableArray<any> {
 		var row = this.toggleRow(array.getItem(index));
-        array.setItem(index,row);
+        array.setItem(index, row);
 		return array;
 	}
 
 	/** get number of items in the array */
-	public count(array: any[]) : number {
-		if(!array) return 0;
-		return  array.length;
+	public count(array: any[]): number {
+		if (!array) return 0;
+		return array.length;
 	}
 	/** get number of tagged items in the array */
-	public countTagged(array: any[]) : number {
-		if(!array) return 0;
+	public countTagged(array: any[]): number {
+		if (!array) return 0;
 		return this.getTaggedRows(array).length;
 	}
 	/** get number of untagged items in the array */
-	public countUntagged(array: any[]) : number {
-		if(!array) return 0;
+	public countUntagged(array: any[]): number {
+		if (!array) return 0;
 		return this.getTaggedRows(array).length;
 	}
 	/** return the tagged rows from the array */
-	public getTaggedRows(array: any[]) : any[] {
+	public getTaggedRows(array: any[]): any[] {
 		var me = this;
-		if(!array) return null;
-		var taggedRows =array.filter(function (x) {
+		if (!array) return null;
+		var taggedRows = array.filter(function (x) {
 			return (x.tag && x.tag.get("value") == me.tagIcon);
 		});
 		return taggedRows;
 	}
 	/** return the untagged rows from the array */
-	public getUnTaggedRows(array: any[]) : any[] {
+	public getUnTaggedRows(array: any[]): any[] {
 		var me = this;
-		var taggedRows =array.filter(function (x) {
+		var taggedRows = array.filter(function (x) {
 			return (x.tag && x.tag.get("value") == me.unTagIcon);
 		});
 		return taggedRows;
@@ -104,7 +104,7 @@ class Sql {
 	//other
 	/** return a sql snipped to fetch a clarion date from the database as a standard date*/
 	public date(field) {
-		return sf("convert(varchar,convert(datetime,{0}-36163),103)",field );
+		return sf("convert(varchar,convert(datetime,{0}-36163),103)", field);
 	}
 }
 
@@ -112,8 +112,8 @@ class Sql {
 class Str {
 
 	/** return a URI encoded string */
-	public fixedEncodeURIComponent(url: string) : string {
-		return encodeURIComponent(url).replace(/[!'()*]/g, function(c) {
+	public fixedEncodeURIComponent(url: string): string {
+		return encodeURIComponent(url).replace(/[!'()*]/g, function (c) {
 			return '%' + c.charCodeAt(0).toString(16);
 		});
 	}
@@ -121,8 +121,8 @@ class Str {
 	/** return a filtered observable array where the named field(property) contains specific text (case insensitive) */
 	public filterArray(data: any[], searchField: string, searchText: string) {
 		searchText = searchText.toLowerCase()
-		var filteredData =data.filter(function (x) {
-			return (x[searchField] && x[searchField].toLowerCase().indexOf(searchText)>=0);
+		var filteredData = data.filter(function (x) {
+			return (x[searchField] && x[searchField].toLowerCase().indexOf(searchText) >= 0);
 		});
 		return new ObservableArray(filteredData);
 	}
@@ -130,23 +130,23 @@ class Str {
 	/** return a filtered observable array where the named fields(properties) contains specific text (case insensitive) */
 	public filterArrayByArray(data: any[], searchField: string[], searchText: string) {
 		searchText = searchText.toLowerCase()
-		var filteredData =data.filter(function (x) {
+		var filteredData = data.filter(function (x) {
 
 			for (var i = 0; i < searchField.length; i++) {
-				if (x[searchField[i]] && x[searchField[i]].toLowerCase().indexOf(searchText)>=0) return true;
+				if (x[searchField[i]] && x[searchField[i]].toLowerCase().indexOf(searchText) >= 0) return true;
 			}
 			return false;
 
 		});
 		return new ObservableArray(filteredData);
 	}
-	
+
 	/** return true if a string contains any item in the substring array) */
-	public containsAny(str: string, substrings: string[]) : boolean {
+	public containsAny(str: string, substrings: string[]): boolean {
         for (var i = 0; i != substrings.length; i++) {
-           if (str.indexOf(substrings[i]) != - 1) return true;
+			if (str.indexOf(substrings[i]) != - 1) return true;
         }
-        return false; 
+        return false;
     }
 
 	/** return a filtered array where the named field(property) contains specific text (case insensitive) */
@@ -158,26 +158,31 @@ class Str {
 
 	/** get the first item from an array where the named field(property) contains specific text (case insensitive) */
 	public getArrayItem(array: any[], searchField: string, searchValue: any) {
-		return this.getArrayItems(array,searchField,searchValue)[0];
+		return this.getArrayItems(array, searchField, searchValue)[0];
 	}
 
 	/** convert an array to and observable array */
-	public observableArray (array?: Array<any>) {
+	public observableArray(array?: Array<any>) {
 		return new ObservableArray(array);
 	}
 
 	/** Extract objects from array  */
-	public getArrayObjects (array: Array<any>, objectName: string) : Array<any> {
+	public getArrayObjects(array: Array<any>, objectName: string): Array<any> {
 		return array.map(function (x) { return x[objectName]; });
 	}
 
 	/** replaces an existing observableArrays data with a new array  */
 	public replaceArray(array: ObservableArray<any>, withArray: any) {
         array.splice(0);
+		this.appendArray(array,withArray)
+	}
+
+	/** appends an existing observableArrays data with a new array  */
+	public appendArray(array: ObservableArray<any>, withArray: any) {
         for (var index = 0; index < withArray.length; index++) {
-                array.push(withArray[index]);   
+			array.push(withArray[index]);
         }
-  }	
+	}
 
 }
 
@@ -185,53 +190,53 @@ class Str {
 class Dt {
 
 	/** convert a date to a string (DD/MM/YYYY) */
-	public dateToStr(date?: Date) : string {
-		if(!date) {
-	        return moment().format('DD/MM/YYYY');
+	public dateToStr(date?: Date): string {
+		if (!date) {
+			return moment().format('DD/MM/YYYY');
 		} else {
-	        return moment(date).format('DD/MM/YYYY');
+			return moment(date).format('DD/MM/YYYY');
 		}
 	}
 
 	/** convert a string (DD/MM/YYYY) to a date */
-	public strToDate(date: string) : Date {
-		if(!date) {
-		   moment().toDate();
+	public strToDate(date: string): Date {
+		if (!date) {
+			moment().toDate();
 		} else {
-		   return moment(date, 'DD/MM/YYYY').toDate();
+			return moment(date, 'DD/MM/YYYY').toDate();
 		}
 	}
 	/** convert a date to a moment object */
 	public strToMoment(date: string) {
-		if(!date) {
-		   return moment();
+		if (!date) {
+			return moment();
 		} else {
-		   return moment(date, 'DD/MM/YYYY');
+			return moment(date, 'DD/MM/YYYY');
 		}
 	}
 	/** convert a date to a clarion date */
-	public clarionDate(date: Date) : number {
-		var oneDay = 24*60*60*1000; // hours*minutes*seconds*milliseconds
+	public clarionDate(date: Date): number {
+		var oneDay = 24 * 60 * 60 * 1000; // hours*minutes*seconds*milliseconds
 		var startDate = new Date("December 28, 1800");
-		var diffDays = Math.round(Math.abs((date.getTime() - startDate.getTime())/(oneDay)))
+		var diffDays = Math.round(Math.abs((date.getTime() - startDate.getTime()) / (oneDay)))
 		return diffDays
 	}
 }
 
 class ViewExt {
 	/** remove the focus from a view object */
-	public clearFocus(view: view.View)  {
-		if(!view) return;
-        if(isAndroid) if(view.android) view.android.clearFocus();
+	public clearFocus(view: view.View) {
+		if (!view) return;
+        if (isAndroid) if (view.android) view.android.clearFocus();
 	}
 	/** hide the soft keyboard from a view object */
-	public dismissSoftInput(view: view.View)  {
-		if(!view) return;
-	  	try {
-	       (<any>view).dismissSoftInput();
-	  	} catch (error) {
-		  
-	  	}
+	public dismissSoftInput(view: view.View) {
+		if (!view) return;
+		try {
+			(<any>view).dismissSoftInput();
+		} catch (error) {
+
+		}
 	}
 }
 
@@ -244,7 +249,7 @@ export interface IValueItem {
 export class ValueList {
 
 	/** this array of value items */
-    private  items: Array<IValueItem>;
+    private items: Array<IValueItem>;
 
 	/** the number of items */
     get length(): number { return this.items.length; }
@@ -264,11 +269,11 @@ export class ValueList {
     }
 
 	/** get the list of value items */
-    public getItems() :Array<IValueItem> {
+    public getItems(): Array<IValueItem> {
         return this.items;
     }
-	
-		/** get an item by its index */
+
+	/** get an item by its index */
     public getItem(index: number): IValueItem {
         if (index < 0 || index >= this.items.length) {
             return null;
@@ -284,7 +289,7 @@ export class ValueList {
         return this.items[index].DisplayMember;
     }
 	/** get an array of the items text field  */
-	public getTextArray () : Array<any> {
+	public getTextArray(): Array<any> {
 		var me = this;
 		return me.items.map(function (x: IValueItem) { return x.DisplayMember; });
 	}
@@ -298,7 +303,7 @@ export class ValueList {
     }
 
 	/** get the items index by its value, use default index if not found else return -1 */
-	
+
     public getIndex(value: any, defaultIndex?: number): number {
         let loop: number;
         for (loop = 0; loop < this.items.length; loop++) {
@@ -306,7 +311,7 @@ export class ValueList {
                 return loop;
             }
         }
-        return defaultIndex == null ? -1 : defaultIndex ;
+        return defaultIndex == null ? -1 : defaultIndex;
     }
 }
 
